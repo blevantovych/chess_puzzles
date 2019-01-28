274fab67e7b5063204a073dd1e06cef39fdaecdf
blevantovych <levantovychbogdan@gmail.com>
2019-01-28 21:38:10 +0200

    dont show solution right away

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Board from './Board'
 import './static/css/chessground.css'
 import './static/css/theme.css'
 import positions from './positions'
-// import './static/css/style.css'
+import Chess from 'chess.js'
+
 
 const IDS_OF_SOLVED_POSITIONS = 'ids_of_solved_positions';
 
 const getPositionId = positions => {
-  debugger;
   let ids = localStorage.getItem(IDS_OF_SOLVED_POSITIONS);
   if (ids) {
     ids = JSON.parse(ids)
   } else {
     ids = []
   }
-  for (var i = 0; i < positions.length; i++) {
-    if (!ids.includes(i)) {
-      return i;
-    }
+  return 0;
+  // for (let i = 0; i < positions.length; i++) {
+  //   if (!ids.includes(i)) {
+  //     return i;
+  //   }
+  // }
+}
+
+const getState = (index = getPositionId(positions)) => {
+  return {
+    fail: false,
+    success: false,
+    moveCount: 0,
+    index,
+    position: positions[index],
+    whichSideMoves: positions[index].condition === '1. ?' ? 'white' : 'black'
   }
 }
 
 class App extends Component {
-  state = {
-    index: getPositionId(positions)
+
+  constructor() {
+    super()
+    this.state = getState()
   }
+
   render() {
     const ids = localStorage.getItem(IDS_OF_SOLVED_POSITIONS)
     const solved = ids ? JSON.parse(ids).length : 0;
@@ -38,10 +52,81 @@ class App extends Component {
           id={this.state.index}
           all={positions.length}
           solved={solved}
-          position={positions[this.state.index]}
-          next={() => this.setState({index: getPositionId(positions)})}
+          position={this.state.position}
+          makeMove={({from, to}) => {
+            const orientation = this.state.position.condition === '1. ?' ? 'w' : 'b'
+            const chess = new Chess();
+            chess.load(`${this.state.position.fen} ${orientation} - - 1 1`)
+            const solutionMoves = this.state.position.solution.replace(/\(.+\)/, '').replace(/\$\d+/, '').replace(/\d+\.+/g, '').trim().split(/\s+/)
+            const chess2 = new Chess();
+            chess2.load(`${this.state.position.fen} ${orientation} - - 1 1`)
+            const isMoveValid = chess.move({from, to})
+
+            if (isMoveValid) {
+              this.setState({
+                position: {
+                  ...this.state.position,
+                  fen: chess.fen().replace(/\s.+/, ''),
+                  condition: this.state.position.condition === '1. ?' ? '1... ?' : '1. ?'
+                },
+              })
+            }
+
+            // const valid = chess2.move(solutionMoves[0], {sloppy: true})
+            // const chess2fen = chess2.move(solutionMoves[0], {sloppy: true})
+            // console.log({valid, chess2fen})
+            console.log(chess2.fen())
+            console.log(solutionMoves[this.state.moveCount])
+            
+            const mCorrect = chess2.move(solutionMoves[this.state.moveCount], {sloppy: true})
+            if (!mCorrect) {
+              console.log('move is not correct')
+              console.log(chess2.fen())
+            } 
+            console.log(chess2.fen())
+
+            if (chess.fen().replace(/\s.+/, '') === chess2.fen().replace(/\s.+/, '')) {
+              console.log('right move')
+              // debugger;
+              if (solutionMoves.length > this.state.moveCount + 1) {
+                setTimeout(() => {
+                  // solutionMoves.shift();
+                  // debugger;
+                  // console.log(chess2.fen())
+                  const isMoveValid = chess2.move(solutionMoves[this.state.moveCount + 1], {sloppy: true})
+  
+                  if (isMoveValid) {
+                    this.setState({
+                      moveCount: this.state.moveCount + 2,
+                      position: {
+                        ...this.state.position,
+                        fen: chess2.fen().replace(/\s.+/, ''),
+                        condition: this.state.position.condition === '1. ?' ? '1... ?' : '1. ?'
+                      },
+                    })
+                  }
+                }, 500)
+
+              } else {
+                this.setState({
+                  success: true
+                })
+              }
+              
+            } else {
+              this.setState({
+                fail: true
+              })
+            }
+          }}
+          whichSideMoves={this.state.whichSideMoves}
+          fail={this.state.fail}
+          success={this.state.success}
+          next={() => {
+            this.setState(getState())
+          }}
           prev={() => {
-            this.state.index > 0 && this.setState({index: this.state.index - 1})
+            this.state.index > 0 && this.setState(getState(this.state.index - 1))
           }}
         />
       </div>
--- src/Board.js
+++ src/Board.js
@@ -10,30 +10,31 @@ export default class Board extends React.Component {
   }
 
   update () {
-    const {fen, condition} = this.props.position;
+    const {fen} = this.props.position;
     const board = document.querySelector('#board')
 
-    const orientation = condition === '1. ?' ? 'white' : 'black'
+    const orientation = this.props.whichSideMoves
     Chessground(board, {
       orientation,
       viewOnly: false,
       fen,
       events: {
-          move: () => {
-            let ids = localStorage.getItem(IDS_OF_SOLVED_POSITIONS);
-            if (ids) {
-              ids = JSON.parse(ids)
-            } else {
-              ids = []
-            }
-
-            this.setState({
-              showSolution: true
-            })
-            if (!ids.includes(this.props.id))
-              ids.push(this.props.id)
-            localStorage.setItem(IDS_OF_SOLVED_POSITIONS, JSON.stringify(ids))
+        move: (from, to) => {
+          this.props.makeMove({from, to})
+          let ids = localStorage.getItem(IDS_OF_SOLVED_POSITIONS);
+          if (ids) {
+            ids = JSON.parse(ids)
+          } else {
+            ids = []
           }
+
+          // this.setState({
+          //   showSolution: true
+          // })
+          if (!ids.includes(this.props.id))
+            ids.push(this.props.id)
+          localStorage.setItem(IDS_OF_SOLVED_POSITIONS, JSON.stringify(ids))
+        }
       }
     });
   }
@@ -63,10 +64,18 @@ export default class Board extends React.Component {
       <div style={{marginTop: '20px'}}>
         {info}
       </div>
-      {this.state.showSolution && <div style={{marginTop: '20px'}}>
+      {(this.props.success || this.props.fail) && <div style={{marginTop: '20px'}}>
         Solution: {solution}
       </div>}
-      <div style={{marginTop: '50px'}}>
+      {this.props.success && <div style={{display: 'flex', alignItems: 'center'}}>
+        <div className="icon success">✓</div>
+        <div>Puzzle solved</div>
+      </div>}
+      {this.props.fail && <div style={{display: 'flex', alignItems: 'center'}}>
+        <div className="icon fail">✗</div>
+        <div>Puzzle failed</div>
+      </div>}
+      <div style={{marginTop: '5px'}}>
         <button className="navigation_button" onClick={this.prev.bind(this)}>&lt;</button>
         <button className="navigation_button" onClick={this.next.bind(this)}>&gt;</button>
       </div>
